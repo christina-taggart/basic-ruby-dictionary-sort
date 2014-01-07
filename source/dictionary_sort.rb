dd5681100f86250fbd2acc44114ce74e788aee8c
Marion <knight499@gmail.com>
2014-01-06 17:17:14 -0800

    Add original files

--- source/dictionary_sort.rb
+++ source/dictionary_sort.rb
@@ -1,5 +1,16 @@
-def dictionary_sort(arr)
-  # Your code here to sort the array
+def dictionary_sort
+  input = "Something good"
+  dictionary = []
+  until input == ""
+    puts "Type a word: "
+    input = gets.chomp
+    dictionary << input if input != ""
+  end
+
+  dictionary.sort_by! {|word| word.downcase}
+  puts "Congratulations! Your dictionary has #{dictionary.length} words:"
+  puts dictionary
 end
 
 # ...your code here to initialize the program and take user input
+dictionary_sort
