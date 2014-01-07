22c8c73678a7ef29e92f3361897c578265327e48
Apprentice <apprentice@dbc29.local>
2014-01-06 17:54:15 -0800

    release 0

--- source/dictionary_sort.rb
+++ source/dictionary_sort.rb
@@ -1,5 +1,15 @@
 def dictionary_sort(arr)
-  # Your code here to sort the array
+
 end
 
-# ...your code here to initialize the program and take user input
+def get_user_input
+  user_input = []
+  user_input << gets.chomp
+  while user_input[-1] != ""
+    user_input << gets.chomp
+  end
+  user_input.pop
+  user_input
+end
+
+get_user_input
