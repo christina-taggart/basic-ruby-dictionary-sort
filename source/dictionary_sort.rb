030348cad864de829d3a1c5f5fab1ca4d44e03bb
Apprentice <apprentice@dbc29.local>
2014-01-06 18:33:52 -0800

    release 1

--- source/dictionary_sort.rb
+++ source/dictionary_sort.rb
@@ -1,15 +1,16 @@
 def dictionary_sort(arr)
-
+  puts arr.sort { |a,b| a.upcase <=> b.upcase }
 end
 
 def get_user_input
   user_input = []
   user_input << gets.chomp
   while user_input[-1] != ""
+    raise "Invalid Input" if /\W|\d/.match(user_input[-1])
     user_input << gets.chomp
   end
   user_input.pop
   user_input
 end
 
-get_user_input
+dictionary_sort(get_user_input)
