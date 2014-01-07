86802ee35f7c8e1497be147269eee9c77780e904
Apprentice <apprentice@DBC-Infinity-3.local>
2014-01-06 17:55:49 -0800

    Wrote a program that asks user to enter words and stores them in an array

--- source/dictionary_sort.rb
+++ source/dictionary_sort.rb
@@ -1,5 +1,18 @@
 def dictionary_sort(arr)
   # Your code here to sort the array
+
+  puts arr
+
 end
 
 # ...your code here to initialize the program and take user input
+def get_user_input
+  dictionary_array = []
+  puts "Enter one word at a time, separated by Enter. When you're done, enter a blank line."
+  user_input = gets.chomp
+  until user_input == ""
+    dictionary_array << user_input
+    user_input = gets.chomp
+  end
+  dictionary_array
+end
