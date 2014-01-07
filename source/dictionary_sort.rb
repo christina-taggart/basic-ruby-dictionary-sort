20c833e5f5f5c845fbd8c70c634bb9e7ea10cdd2
Apprentice <apprentice@dbc26.local>
2014-01-06 17:16:39 -0800

    Create a method to sort inputted words

--- source/dictionary_sort.rb
+++ source/dictionary_sort.rb
@@ -1,5 +1,30 @@
+# ask for words from user
+# put words into an array
+# Stop when a blank line is entered
+# sort array alphibetically
+
+
+
+
+
 def dictionary_sort(arr)
-  # Your code here to sort the array
+  arr.sort_by! {|word| word.downcase}
 end
+
+words = []
+word = " "
+until word == ""
+  print words.length == 0 ? "Type a word " : "Type another word (or press enter to finish) "
+  word = gets.chomp
+  unless word == ""
+    words << word
+  end
+end
+
+
+dictionary_sort(words)
+puts "Congratulations!  Your dictionary has #{words.count} words:"
+words.each {|x| puts x}
+
 
 # ...your code here to initialize the program and take user input
