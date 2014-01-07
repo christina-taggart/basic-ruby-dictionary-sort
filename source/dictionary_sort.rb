b86b172b164b7bb498a0dca614ce8717b20d2549
Apprentice <apprentice@dbc27.local>
2014-01-06 17:32:15 -0800

    Completing dictionary program

--- source/dictionary_sort.rb
+++ source/dictionary_sort.rb
@@ -1,5 +1,21 @@
-def dictionary_sort(arr)
+def dictionary_sort(*)
   # Your code here to sort the array
+  puts "Type a word:"
+  words = []
+  while input = gets.chomp
+    if input != ""
+      words << input
+    else
+      break
+    end
+
+    puts "Type another word (or press enter to finish):"
+  end
+
+  words.sort_by! { |word| word.downcase }
+  puts "Congratulations! Your dictionary has " + words.length.to_s + " words:"
+  words.each { |word| puts word}
 end
 
 # ...your code here to initialize the program and take user input
+dictionary_sort
