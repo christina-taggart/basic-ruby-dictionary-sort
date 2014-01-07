b7dd875d02a035d5d49f2228a28a986ed4073516
Apprentice <apprentice@dbc05.local>
2014-01-06 17:27:32 -0800

    Release 0: Store User Input

--- source/dictionary_sort.rb
+++ source/dictionary_sort.rb
@@ -3,3 +3,15 @@ def dictionary_sort(arr)
 end
 
 # ...your code here to initialize the program and take user input
+def add_words
+  dictionary = []
+  word = ""
+  until word == "\n"
+    puts "Enter a word"
+    word = gets
+    dictionary << word.chomp
+  end
+  puts dictionary
+end
+
+add_words
