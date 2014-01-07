c38f2c2c0d539b57310d6906e5bbad89d2e25188
Apprentice <apprentice@dbc28.local>
2014-01-06 17:57:42 -0800

    Dictionary accepts input and sorts alphabetically, regardless of case, but does not accept numbers.

--- source/dictionary_sort.rb
+++ source/dictionary_sort.rb
@@ -1,5 +1,24 @@
 def dictionary_sort(arr)
-  # Your code here to sort the array
+  new_arr = arr.sort_by { |a| a.downcase }
+  puts "Congratulations! Your dictionary has #{new_arr.size} words:"
+  new_arr.each {|s| puts s}
 end
 
-# ...your code here to initialize the program and take user input
+arr = []
+user_input = " "
+
+until user_input == ""
+  if user_input == " "
+    puts "Type a word:"
+  else
+    puts "Type another word (or press enter to finish):"
+  end
+  user_input = gets.chomp
+  if user_input.match(/\d/)
+    puts "Word cannot contain number."
+  else
+    arr << user_input unless user_input == ""
+  end
+end
+
+dictionary_sort(arr)
